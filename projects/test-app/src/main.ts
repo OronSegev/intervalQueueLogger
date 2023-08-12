53f42e57df972495029716e58d7f729491b05f94
Oron Segev <segev.oron@gmail.com>
2023-08-12 22:23:33 +0300

    add some config example

--- projects/test-app/src/main.ts
+++ projects/test-app/src/main.ts
@@ -12,9 +12,32 @@ bootstrapApplication(AppComponent, {
       HttpClientModule,
       BrowserAnimationsModule,
       CommonModule,
+      // using defualt config - console log as defualt target , and direct logger - no queue
       IntervalQueueLoggerModule.forRoot({
         isProduction: true,
       })
     ),
   ],
 });
+
+// different configuration - using interval queue and setting interval for 5 sec
+// and using local Storage as target
+
+// IntervalQueueLoggerModule.forRoot({
+//   isProduction: true,
+//   queue: {
+//     interval: 5000 // time in milisecound
+//   },
+//   target: 'localStorage'
+// })
+
+// example for different message format
+
+// IntervalQueueLoggerModule.forRoot({
+//   isProduction: true,
+//   queue: {
+//     interval: 5000 // time in milisecound
+//   },
+//   target: 'localStorage',
+//   messageFormat: '[{message} - {timeStamp}, {stackTrace}]'
+// })
